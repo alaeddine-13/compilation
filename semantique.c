506d0a8560752ee91da4a8031dc3d2c135311da3
Alaeddine Abdessalem <alaeddine-13@live.fr>
2021-05-07 11:02:08 +0100

    final

--- semantique.c
+++ semantique.c
@@ -201,7 +201,6 @@ void verifierVarInitialise (char* nom, int nbline){
 
 void finProcedure(int nbline)
 {
-    printf("g_ifproc: %d\n", g_IfProc);
     NOEUD tmp_table;
     if (g_IfProc == 1){
         g_IfProc = 0;
